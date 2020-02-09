7f05c54fa0ed9a851307949537075af9d76d67a2
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-08 20:07:37 -0600

    added constructor

--- src/Pendulum/SimplePendulum.java
+++ src/Pendulum/SimplePendulum.java
@@ -3,6 +3,17 @@ package Pendulum;
 import javax.swing.*;
 import java.awt.*;
 
-public class SimplePendulum {
+public class SimplePendulum extends Canvas{
+    private double mass;        // kg
+    private double length;      // m
+    private double angle;       // rad
+    private double angleAcc = 0;
+    private double angleVel = 0;
+
+    public SimplePendulum(double mass, double length, double angle){
+        this.mass = mass;
+        this.length = length;
+        this.angle = angle;
+    }
 
 }
