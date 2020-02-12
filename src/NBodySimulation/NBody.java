1efacc6f3045f5c964564dfc328dbb7e5725e0f1
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-11 23:06:23 -0600

    added read planet method and test

--- src/NBodySimulation/NBody.java
+++ src/NBodySimulation/NBody.java
@@ -8,4 +8,16 @@ public class NBody {
         double radius = in.readDouble();
         return radius;
     }
+
+    public static Planet[] readPlanets(String inFile){
+        In in = new In(inFile);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        Planet[] planet = new Planet[num];
+
+        for(int i = 0; i < num; i++){
+            planet[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(), in.readDouble(),in.readString());
+        }
+        return planet;
+    }
 }
