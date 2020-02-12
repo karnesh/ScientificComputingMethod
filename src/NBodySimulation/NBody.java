db1e26253aa6a98d3dce4c992e316c1adf959638
Karnesh Jain <jain.karnesh@gmail.com>
2020-02-11 22:00:53 -0600

    Added read file method in NBody class

--- src/NBodySimulation/NBody.java
+++ src/NBodySimulation/NBody.java
@@ -1,4 +1,11 @@
 package NBodySimulation;
 
 public class NBody {
+
+    public static double readRadius(String inFile){
+        In in = new In(inFile);
+        int num = in.readInt();
+        double radius = in.readDouble();
+        return radius;
+    }
 }
